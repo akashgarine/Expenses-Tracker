d9f9a0180c42039d884c37fd65d639867dcfe0f3
akashgarine <akashgarine123@gmail.com>
2024-10-23 01:19:46 +0530

    2 commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/rtfp');
+mongoose.connect('mongodb://127.0.0.1:27017/Expensestracker');
 
 //signup
 app.post("/usersignup", (req, res) => {
